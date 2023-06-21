5b69f56ab56c30e467b84d8f0a86c88f715b31fd
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-06-21 16:46:45 +0300

    Set up Postgres DB Docker configuration and application DB context

---
+++ webapi/Infrastructure/Database/TrainComponentsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Infrastructure.Database
+{
+    public class TrainComponentsDbContext : DbContext
+    {
+        public TrainComponentsDbContext(DbContextOptions<TrainComponentsDbContext> options) : base(options) { }
+    }
+}
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using webapi.Infrastructure.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure database context
+var connStr = builder.Configuration.GetConnectionString("PostgresConnectionString");
+builder.Services.AddDbContext<TrainComponentsDbContext>(options => { 
+    options.UseNpgsql(connStr);
+});
 
 var app = builder.Build();
 
